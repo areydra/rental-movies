a26190406085015b7a673944273db278b75e0b89
Areydra <areydra@gmail.com>
2019-08-25 17:39:16 +0700

    add comentar documentation at paginate.js

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -2,7 +2,11 @@ import _ from 'lodash'
 
 const paginate = (items, currentPage, limitPage) => {
     const index = (currentPage - 1) * limitPage
-    return _(items).slice(index).take(limitPage).value()
+    return _(items).slice(index).take(limitPage).value() 
+    //_(items) masukan items kedalam lodash
+    //.slice(index) mulai data dari index, bisa juga dengan cara .slice(items, index)
+    //.take(limitPage) ambil data sesuai limit, 
+    //.value() convert dari lodash obj menjadi reguler array
 } 
 
 export default paginate
